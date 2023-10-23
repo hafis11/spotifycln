dc983e065d32c806b20b00728fb6c2356af42601
Hafis muhammed <hafismuhammed984@gmail.com>
2023-10-23 19:11:04 +0530

    update meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,11 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <meta name="application-name" content="PWA App" />
+                <meta name="application-name" content="Spotify Clone" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-                <meta name="apple-mobile-web-app-title" content="PWA App" />
-                <meta name="description" content="Best PWA App in the world" />
+                <meta name="apple-mobile-web-app-title" content="Spotify Clone" />
+                <meta name="description" content="🎧 Check out my latest educational project - a full-fledged Spotify Clone built with NEXT.JS! " />
                 <meta name="format-detection" content="telephone=no" />
                 <meta name="mobile-web-app-capable" content="yes" />
                 <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -35,17 +35,17 @@ export default function Document() {
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
                 <meta name="twitter:card" content="summary" />
-                <meta name="twitter:url" content="https://yourdomain.com" />
-                <meta name="twitter:title" content="PWA App" />
-                <meta name="twitter:description" content="Best PWA App in the world" />
-                <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
+                <meta name="twitter:url" content="https://spotifycln-o6br.vercel.app" />
+                <meta name="twitter:title" content="Spotify Clone" />
+                <meta name="twitter:description" content="🎧 Check out my latest educational project - a full-fledged Spotify Clone built with NEXT.JS! " />
+                <meta name="twitter:image" content="https://spotifycln-o6br.vercel.app/screenshot/theme.png" />
                 <meta name="twitter:creator" content="@DavidWShadow" />
                 <meta property="og:type" content="website" />
-                <meta property="og:title" content="PWA App" />
-                <meta property="og:description" content="Best PWA App in the world" />
-                <meta property="og:site_name" content="PWA App" />
-                <meta property="og:url" content="https://yourdomain.com" />
-                <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+                <meta property="og:title" content="Spotify Clone" />
+                <meta property="og:description" content="🎧 Check out my latest educational project - a full-fledged Spotify Clone built with NEXT.JS! " />
+                <meta property="og:site_name" content="Spotify Clone" />
+                <meta property="og:url" content="https://spotifycln-o6br.vercel.app" />
+                <meta property="og:image" content="https://spotifycln-o6br.vercel.app/screenshot/theme.png" />
             </Head>
             <body>
                 <Main />
